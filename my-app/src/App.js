df8e7791ae7b8c74a9efacf773ac1b0f70c204fa
hardik200404 <Polenati.hardik@gmail.com>
2024-10-24 00:55:30 +0530

    added my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItmes from "./components/ExpenseItmes"
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+      <ExpenseItmes></ExpenseItmes>
+    </div>
+  )
+}
+
+export default App
---
+++ my-app/src/components/ExpenseItmes.js
@@ -0,0 +1,10 @@
+function ExpenseItmes() {
+    return (
+      <div >
+        <li>Food - 40Rs</li>
+        <li>Petrol - 50Rs</li>
+      </div>
+    )
+  }
+  
+  export default ExpenseItmes
