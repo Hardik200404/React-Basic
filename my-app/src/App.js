149646f336af4a108dfd5ed86d365caaca5d2a94
hardik200404 <Polenati.hardik@gmail.com>
2024-10-24 16:02:39 +0530

    updated component with map and key attribute

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
 import ExpenseItems from "./components/ExpenseItems"
 
 function App() {
+  let expenses = [
+    {"id": 1, "item": "Food - 40Rs"},
+    {"id": 2, "item": "Petrol - 50Rs"}
+  ]
   return (
     <div className="App">
       <p>Hello World</p>
-      <ExpenseItems item = "Food - 40Rs"></ExpenseItems>
-      <ExpenseItems item = "Petrol - 50Rs"></ExpenseItems>
+      {
+        expenses.map(expense => {
+          return (
+          <ExpenseItems key = {expense.id} item = {expense.item}></ExpenseItems>
+          )
+        })
+      }
     </div>
   )
 }
